30925eff370c6d52bd49adc9451b3374d2029620
wangkun <wangkun@woodemi.com>
2021-06-09 17:25:27 +0800

    Add Windows project
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <quick_notify/quick_notify_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  QuickNotifyPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("QuickNotifyPlugin"));
+}
