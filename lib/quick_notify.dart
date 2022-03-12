cac77e87a7770d32c9b234f6abde4b75e85913d4
wangkun <wangkun@woodemi.com>
2022-03-12 16:00:48 +0800

    Extract platform interface and method_channel implementation
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- lib/quick_notify.dart
+++ lib/quick_notify.dart
@@ -1,15 +1,26 @@
+import 'src/method_channel_quick_notify.dart';
+import 'src/quick_notify_platform_interface.dart';
 
-import 'package:flutter/services.dart';
+bool _manualDartRegistrationNeeded = true;
+
+QuickNotifyPlatform get _platform {
+  // This is to manually endorse Dart implementations until automatic
+  // registration of Dart plugins is implemented. For details see
+  // https://github.com/flutter/flutter/issues/52267.
+  if (_manualDartRegistrationNeeded) {
+    // Only do the initial registration if it hasn't already been overridden
+    // with a non-default instance.
+    QuickNotifyPlatform.instance = MethodChannelQuickNotify();
+    _manualDartRegistrationNeeded = false;
+  }
+
+  return QuickNotifyPlatform.instance;
+}
 
 class QuickNotify {
-  static const MethodChannel _channel =
-      const MethodChannel('quick_notify');
-
   static void notify({
     String? content,
-  }) {
-    _channel.invokeMethod('notify', {
-      'content': content,
-    });
-  }
+  }) => _platform.notify(
+    content: content,
+  );
 }
